ac930eaba2e8144a43a4346d65f8abdf80990f02
Egor Kazantsev <3gork4z4ncev@gmail.com>
2022-08-10 21:56:45 +0500

    Chapter 2. Design taco page and controller, order page and controller, and converter

---
+++ src/main/java/org/egorkazantsev/tacocloud/controller/OrderController.java
@@ -0,0 +1,30 @@
+package org.egorkazantsev.tacocloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.egorkazantsev.tacocloud.domain.TacoOrder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+@SessionAttributes("tacoOrder")
+public class OrderController {
+
+    @GetMapping("/current")
+    public String orderForm() {
+        return "order_form.html";
+    }
+
+    @PostMapping
+    public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+        log.info("Order submitted: {}", order);
+        sessionStatus.isComplete();
+
+        return "redirect:/";
+    }
+}
